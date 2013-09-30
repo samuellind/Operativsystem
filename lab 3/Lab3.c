78195ca99f83faf51107f72a7bc74f44be6af70d
Sam <samuel_lind@hotmail.com>
2013-09-30 15:02:35 +0200

    lab3 final

--- lab 3/Lab3.c
+++ lab 3/Lab3.c
@@ -6,8 +6,8 @@
 #include <assert.h>
 #include <unistd.h>
 
-#define INCREMENT 50
-char *array;
+#define INCREMENT 20
+//char *array;
 
 /* returnera tidskillnaden mellan tv0 och tv1 i ms */
 int difftod(struct timeval * tv0, struct timeval * tv1){
@@ -21,23 +21,24 @@ int difftod(struct timeval * tv0, struct timeval * tv1){
 	ta tid på den. 0 har returnerats om allt gick bra, -1 annars.
 */
 int createbigthing(long int storlek){
-	
-        /* Skapa ett stort objekt med någon metod, till exemepel malloc. */
-      
-       char *array =  malloc(storlek*oneMeg);
+	int megs;
+    char *array =  malloc(megs=storlek*oneMeg);
 	if(array == NULL)
 		{
 		printf("out of memory\n");
 		return -1;
 		}
-	else 
-	return 0;
-	int i;
-       for(i= 0; i < 160*storlek; i+= 16*storlek){
+	else { 
+	int i, x = sysconf(_SC_PAGESIZE);
+       for(i = 0; i < megs; i += x){
+		   *(array+i) = 'a';
        
+       
+       }
        free(array);
        array = NULL;
        }
+       return 0;
 }
 
 
@@ -49,11 +50,11 @@ int main(){
 	struct timezone tzp;
 
         /*  Skapa objekt med olika storlekar (i MB) och ta tid*/
-	for(i=INCREMENT;i<=400;i=i+INCREMENT){
+	for(i=INCREMENT;i<=15000;i=i+INCREMENT){
         	rtn = gettimeofday(&tv0, &tzp); /* set timer T0 */
         	createbigthing(i);
 		rtn = gettimeofday(&tv1, &tzp); /* read time T1 */
-		printf("%d msek\n",difftod(&tv0,&tv1));
+		printf("%ld %d \n", i, difftod(&tv0,&tv1));
 		}
 	return 0;
 }
